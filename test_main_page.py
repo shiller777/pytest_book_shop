ea2b332763a3364be8278ad23e0f4cd8e7456c00
shiller777 <lalala@lala.la>
2020-06-14 21:44:04 +0300

    added login page

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,11 @@
 from .pages.main_page import MainPage
-
-link = "http://selenium1py.pythonanywhere.com/"
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
 	link = "http://selenium1py.pythonanywhere.com/"
-	page = MainPage(browser, link)
-	page.open()
-	page.should_be_login_link()
+	main_page = MainPage(browser, link)
+	main_page.open()
+	main_page.should_be_login_link()
+	main_page.go_to_login_page()
+	login_page = LoginPage(browser, browser.current_url)
+	login_page.should_be_login_page()
